cb6c442561596473bac0b862b75c75ffa99f6f53
Jose Carvajal <josecarcar1@gmail.com>
2018-09-21 16:15:06 -0500

    Change several text field to select tags for project

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,4 +9,49 @@ class Project < ApplicationRecord
 	validates :status, presence: true
 	has_many :students
 	accepts_nested_attributes_for :user
+
+
+	enum client: [
+		"Departamento del Tec", 
+		"Desarrollo propio",
+		"Gobierno",
+		"Industria Privada",
+		"Organización Social",
+		"Profesor de la clase"
+	]
+
+	enum field: [
+		"Biotecnología y alimentos",
+		"Energía",
+		"Nanotecnología",
+		"Ingeniería",
+		"Tecnologías de información",
+		"Comunicación y eletrónica",
+		"Sostenibilidad",
+		"Emprendimiento de base tecnológica",
+		"Otra"
+	]
+
+	enum kind: [
+		"Agrobiotecnología",
+		"Automatización de proceso",
+		"Bionegocios",
+		"Diseño Industrial",
+		"Electrónica-Hardware",
+		"Ingeniería Civil",
+		"Ingeniería de Alimentos",
+		"Ingeniería Industrial",
+		"Ingeniería Mecánica",
+		"Ingeniería Mecatrónica",
+		"Ingeniería Química",
+		"Modelo Arquitectónico",
+		"Modelo de negocio",
+		"Multidiscplinario",
+		"Nanotecnología",
+		"Proceso de producción",
+		"Química-Bioquímica",
+		"Sistemas Embebidos",
+		"Software",
+		"Otro"
+	], _prefix: true
 end
