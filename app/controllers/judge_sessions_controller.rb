165646594c5f9ce21e7066783d1d4dd6a49a3294
Jose Carvajal <josecarcar1@gmail.com>
2018-09-20 20:25:54 -0500

    Added extra condition for judge session redirection

--- app/controllers/judge_sessions_controller.rb
+++ app/controllers/judge_sessions_controller.rb
@@ -6,8 +6,13 @@ class JudgeSessionsController < ApplicationController
     # this method is in charge of redirecting to judge profile if info is valid
     def create
     	@user = login(params[:email], params[:password])
-    	if @user && @user.role == "judge"
-    		redirect_to @user.judge
+    	if @user 
+            if @user.role == "judge"
+    		      redirect_to @user.judge
+            else
+              flash[:danger] = "No tienes los permisos para entrar a esta ruta"
+              render 'new'
+            end
     	else
     		flash[:danger] = "Usuario o contraseña incorrectos"
     		render 'new'
