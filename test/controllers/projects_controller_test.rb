1e2540664b8dd9b31e28e0fd18f080fe65cb0d64
jvazquez96 <jorgevzqz6@gmail.com>
2018-09-22 16:48:25 -0500

    Some test are still failing :(

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -16,11 +16,11 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get new_project_url
     assert_response :success
-  #end
+  end
 
   test "should create project" do
     assert_difference('Project.count') do
-		post signup_proyecto_url, params: { user: {email: @user.email, password: @user.password, role: @user.role, password_confirmation: "password", project_attributes: { abstract: @project.abstract, client: @project.client, field: @project.field, name: @project.name, kind: @project.kind, video_url: @project.video_url}}}
+      post signup_proyecto_url, params: { user: {email: @user.email, password: @user.password, role: @user.role, password_confirmation: "password", project_attributes: { abstract: @project.abstract, client: @project.client, field: @project.field, name: @project.name, kind: @project.kind, video_url: @project.video_url}}}
     end
     assert_redirected_to project_url(Project.last)
   end
