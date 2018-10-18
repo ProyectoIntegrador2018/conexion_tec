6575c03f39d295f576212da82306fb952c59e185
jvazquez96 <jorgevzqz6@gmail.com>
2018-10-17 19:38:21 -0500

    If text field is already selected display the hidden text field

--- app/assets/javascripts/score_field.js
+++ app/assets/javascripts/score_field.js
@@ -1,4 +1,12 @@
 $(document).on('turbolinks:load',function() {
+
+
+	var status = $('#user_project_attributes_status').val();
+
+	if (status == "Calificado") {
+		document.getElementById("scoreDiv").style.display = "block";
+	}
+
 	$('#user_project_attributes_status').on('change', function() {
 		var scoreFields = document.getElementById("scoreDiv");
 		if ($('#user_project_attributes_status').val() == "Calificado") {
