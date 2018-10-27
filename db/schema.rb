2e893f99e20049612085ec6f7152f05bd96312c7
jvazquez96 <jorgevzqz6@gmail.com>
2018-10-27 17:10:22 -0500

    Update questions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_27_211827) do
+ActiveRecord::Schema.define(version: 2018_10_27_215842) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -100,12 +100,12 @@ ActiveRecord::Schema.define(version: 2018_10_27_211827) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.integer "scale"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.string "title"
     t.index ["category_id"], name: "index_questions_on_category_id"
   end
 
