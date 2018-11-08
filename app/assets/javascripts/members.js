beb870845b1f2fb40eaba26f24858dd59fe22e0d
jvazquez96 <jorgevzqz6@gmail.com>
2018-11-07 18:34:34 -0600

    Students works

--- app/assets/javascripts/members.js
+++ app/assets/javascripts/members.js
@@ -1,5 +1,16 @@
 $(document).on('turbolinks:load', function() {
+
+	var limit_students = 6;
+	var count = 0;
 	$('#add_student').on('click', function() {
-		alert("ihuasdiauhsd")
+		if (count < 6) {
+			var parent = document.getElementById("members_form")
+			var form = document.createElement('div')
+			form.innerHTML = '<label for="user_project_attributes_students__nombre">Nombre</label><input class="form-control" type="text" name="user[project_attributes][students][][name]" id="user_project_attributes_students__name" /> <label for="user_project_attributes_students__correo_electronico">Correo electronico</label> <input class="form-control" type="text" name="user[project_attributes][students][][email]" id="user_project_attributes_students__email" /> <label for="user_project_attributes_students__mátricula">Mátricula</label> <input class="form-control" type="text" name="user[project_attributes][students][][enrollment]" id="user_project_attributes_students__enrollment" /> <label for="user_project_attributes_students__carrera">Carrera</label> <input class="form-control" type="text" name="user[project_attributes][students][][major]" id="user_project_attributes_students__major" />'
+			parent.appendChild(form)
+			count += 1
+		} else {
+			alert("Solamente se permiten hasta 6 alumnos")
+		}
 	});
 });
