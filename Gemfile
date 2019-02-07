15f01e264f347d6f34c64f356745bd518a615228
Santiago Sandoval <santiago_st_95@hotmail.com>
2019-02-06 23:25:32 -0600

    DB Changed to MySQL

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
-source 'https://rubygems.org'
+source 'http://rubygems.org'
+# Changed https to http due to a problem
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.4.2'
@@ -13,7 +14,8 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
-
+# Use mysql2 as the database for Active Record
+gem 'mysql2'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
@@ -58,8 +60,6 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-  # Use sqlite3 as the database for Active Record
-  gem 'sqlite3'
   gem "factory_bot_rails"
   gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'  
   gem "rails-erd"
@@ -74,7 +74,7 @@ group :test do
 end
 
 group :production do
-  gem 'pg', '0.20.0'
+  #gem 'pg', '0.20.0'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- db/schema.rb
+++ db/schema.rb
@@ -12,60 +12,60 @@
 
 ActiveRecord::Schema.define(version: 2018_11_15_194623) do
 
-  create_table "categories", force: :cascade do |t|
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "evaluation_questions", id: false, force: :cascade do |t|
-    t.integer "evaluation_id", null: false
-    t.integer "question_id", null: false
+  create_table "evaluation_questions", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "evaluation_id", null: false
+    t.bigint "question_id", null: false
     t.integer "result", default: 0
     t.index ["evaluation_id"], name: "index_evaluation_questions_on_evaluation_id"
     t.index ["question_id"], name: "index_evaluation_questions_on_question_id"
   end
 
-  create_table "evaluations", force: :cascade do |t|
-    t.decimal "total", default: "0.0"
+  create_table "evaluations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.decimal "total", precision: 10, default: "0"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "project_id"
-    t.integer "judge_id"
+    t.bigint "project_id"
+    t.bigint "judge_id"
     t.index ["judge_id"], name: "index_evaluations_on_judge_id"
     t.index ["project_id"], name: "index_evaluations_on_project_id"
   end
 
-  create_table "event_dates", force: :cascade do |t|
+  create_table "event_dates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "event"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "expertise_areas", force: :cascade do |t|
+  create_table "expertise_areas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "area"
   end
 
-  create_table "expertise_areas_judges", force: :cascade do |t|
-    t.integer "expertise_area_id"
-    t.integer "judge_id"
+  create_table "expertise_areas_judges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "expertise_area_id"
+    t.bigint "judge_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["expertise_area_id"], name: "index_expertise_areas_judges_on_expertise_area_id"
     t.index ["judge_id"], name: "index_expertise_areas_judges_on_judge_id"
   end
 
-  create_table "judges", force: :cascade do |t|
+  create_table "judges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.boolean "has_tablet"
     t.integer "department"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_judges_on_user_id"
   end
 
-  create_table "professors", force: :cascade do |t|
+  create_table "professors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "enrollment"
     t.string "email"
     t.integer "department"
@@ -73,12 +73,11 @@ ActiveRecord::Schema.define(version: 2018_11_15_194623) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "course_code"
-    t.boolean "semestre_i", default: false
-    t.integer "project_id"
+    t.bigint "project_id"
     t.index ["project_id"], name: "index_professors_on_project_id"
   end
 
-  create_table "projects", force: :cascade do |t|
+  create_table "projects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.integer "field"
     t.integer "client"
@@ -86,18 +85,18 @@ ActiveRecord::Schema.define(version: 2018_11_15_194623) do
     t.string "video_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.integer "status", default: 0
-    t.decimal "score", default: "0.0"
-    t.string "reason"
+    t.decimal "score", precision: 10, default: "0"
     t.boolean "assistance", default: false
     t.boolean "active", default: false
-    t.integer "category_id"
-    t.integer "expertise_area_id"
+    t.string "reason"
+    t.bigint "category_id"
+    t.bigint "expertise_area_id"
     t.integer "num_evaluations", default: 0
     t.integer "num_assignments", default: 0
     t.string "description"
-    t.integer "event_date_id"
+    t.bigint "event_date_id"
     t.string "stand"
     t.index ["category_id"], name: "index_projects_on_category_id"
     t.index ["event_date_id"], name: "index_projects_on_event_date_id"
@@ -105,16 +104,17 @@ ActiveRecord::Schema.define(version: 2018_11_15_194623) do
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
-  create_table "questions", force: :cascade do |t|
+  create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "category_id"
+    t.bigint "category_id"
     t.string "title"
     t.index ["category_id"], name: "index_questions_on_category_id"
   end
 
-  create_table "students", force: :cascade do |t|
+  create_table "students", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "enrollment"
@@ -125,7 +125,7 @@ ActiveRecord::Schema.define(version: 2018_11_15_194623) do
     t.index ["project_id"], name: "index_students_on_project_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
