e578203f129b860d34c8c2ed164140f3ed78931f
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2019-02-28 14:55:40 -0600

    Implemented new,edit,delete methods

--- app/controllers/admin/departments_controller.rb
+++ app/controllers/admin/departments_controller.rb
@@ -1,26 +1,50 @@
 class Admin::DepartmentsController < Admin::BaseController
 
-	before_action :set_department, only: [:edit, :update]
+	before_action :set_department, except: [:index, :create]
 
 	def index
 		@departments = Department.all
 	end
 
 	def edit
+    @url = admin_department_path
 	end
+
+  def new
+    @url = admin_departments_path
+  end
 
 	def update
 		if @department.update_attributes(department_params)
 			flash[:success] = "Departamento actualizado"
 			redirect_to action: 'index'
 		else
+      @url = admin_department_path
 			flash.now[:danger] = "Error al actualizar departamento"
 			render 'edit'
 		end
 	end
 
+  def create
+    @department = Department.new(department_params)
+    if @department.save
+      flash[:success] = "Departamento creado"
+      redirect_to action: 'index'
+    else
+      @url = admin_departments_path
+      flash.now[:danger] = "Error al crear departamento"
+      render 'new'
+    end
+  end
+
+  def destroy
+    @department.delete
+    flash[:success] = "Departamento eliminado"
+    redirect_to action: 'index'
+  end
+
 	def set_department
-		@department = Department.find(params[:id])
+		@department = params[:id].present? ? Department.find(params[:id]) : Department.new
 	end
 
 	private
