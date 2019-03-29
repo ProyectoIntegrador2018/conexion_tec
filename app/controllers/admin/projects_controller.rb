8d3502dd3bec0303e62b5fe2174e7767ffd2719f
abrilgzz <abril.gonzalezr@gmail.com>
2019-03-29 14:23:10 -0600

    admin CRUD on projects, and approve or reject

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProjectsController < Admin::BaseController
-	before_action :set_project, only: [:new, :show, :edit, :update, :destroy]
+	before_action :set_project, only: [:new, :show, :edit, :update, :destroy, :approve, :reject]
 
 	def show
 	end
@@ -14,10 +14,26 @@ class Admin::ProjectsController < Admin::BaseController
 	end
 
 	def create
-		@project = Project.new(project_params)
+		student = User.find_by(email: project_params["student_id"])
+		professor = User.find_by(email: project_params["professor_id"])
+
+		if professor.nil?
+			prof_instance = Professor.create(department_id: 4)
+			professor = User.create(email: project_params["professor_id"],
+                                userable_type: 'Professor',
+                                userable_id: prof_instance.id)
+		end
+		@project = Project.new(project_params.except(:student_id, :professor_id))
+		@project.student_id = student.userable_id
+		@project.professor_id = professor.userable_id
+		@project.edition_id = Edition.last.id
+		@project.status_id = Status.first.id
+
 		if @project.save
 			flash[:success] = "Proyecto creado exitosamente!"
+			redirect_to action: 'index'
 		else
+			flash[:error] = "Error al crear el proyecto"
 			render 'new'
 		end
 	end
@@ -29,10 +45,38 @@ class Admin::ProjectsController < Admin::BaseController
 	def update
 		if @project.update_attributes(project_params)
 			flash[:success] = "Información del proyecto actualizada"
-			redirect_to edit_admin_project_path(@project)
+			redirect_to action: 'index'
 		else
 			flash[:error] = "Error"
 			render "edit"
+		end
+	end
+
+	def destroy
+		@project.delete
+		flash[:success] = "Proyecto eliminado"
+		redirect_to admin_projects_path
+	end
+
+	def approve
+		@project.status_id = 4
+		if @project.save
+			flash[:success] = "Proyecto aprobado"
+			redirect_to admin_projects_path
+		else
+			flash[:error] = "Error al aprobar proyecto"
+			redirect_to admin_projects_path
+		end
+	end
+
+	def reject
+		@project.status_id = 3
+		if @project.save
+			flash[:success] = "Proyecto rechazado"
+			redirect_to admin_projects_path
+		else
+			flash[:error] = "Error al rechazar proyecto"
+			redirect_to admin_projects_path
 		end
 	end
 
@@ -43,8 +87,17 @@ class Admin::ProjectsController < Admin::BaseController
 
 		def project_params
 			params.require(:project).permit(
-				:name, :field_id, :client_id, :category_id, :expertise_area_id, :abstract, :video_url,
-				:description, :status_id, :selection_score)
+				:name,
+				:field_id, 
+				:client_id, 
+				:category_id, 
+				:expertise_area_id, 
+				:abstract, 
+				:video_url,
+				:description,
+				:selection_score,
+				:student_id,
+				:professor_id)
 		end
 
 end
