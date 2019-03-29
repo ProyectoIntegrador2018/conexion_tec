46669e34fffba0aad3301ba3484db1cfba9e6282
abrilgzz <abril.gonzalezr@gmail.com>
2019-03-29 15:11:15 -0600

    admin can approve/reject judge

--- app/controllers/admin/judges_controller.rb
+++ app/controllers/admin/judges_controller.rb
@@ -1,5 +1,5 @@
 class Admin::JudgesController <  Admin::BaseController
-    before_action :set_judge, only: [:destroy]
+    before_action :set_judge, only: [:destroy, :approve, :reject]
 
     def index
         @judges = Judge.all
@@ -11,6 +11,30 @@ class Admin::JudgesController <  Admin::BaseController
         flash[:success] = "Juez Eliminado"
         redirect_to admin_judges_path
     end
+
+    def approve
+        @user.authorized = 1
+		if @user.save
+			flash[:success] = "Juez aprobado"
+			redirect_to admin_judges_path
+		else
+			flash[:error] = "Error al aprobar el juez"
+			redirect_to admin_judges_path
+		end
+	end
+
+	def reject
+		@user.authorized = 0
+		if @user.save
+			flash[:success] = "Juez rechazado"
+			redirect_to admin_judges_path
+		else
+			flash[:error] = "Error al rechazar el juez"
+			redirect_to admin_judges_path
+		end
+	end
+
+
     
     private
         def set_judge
