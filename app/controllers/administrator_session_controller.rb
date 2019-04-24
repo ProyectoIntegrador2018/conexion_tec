d8b25e17845e07d54e012fe479272b196b7c8075
Santiago Sandoval <santiago_st_95@hotmail.com>
2019-04-24 18:20:13 -0500

    Rebase

--- app/controllers/administrator_session_controller.rb
+++ app/controllers/administrator_session_controller.rb
@@ -1,22 +1,30 @@
 class AdministratorSessionController < ApplicationController
+	include AuthHelper
 
 	def new
-	end
+        if current_user && current_user.admin?
+            redirect_to admin_profile_path
+        else
+            @login_url = get_login_url(authorize_admin_url)
+        end
+    end
 
 	def create
-		user = login(params[:email], params[:password])
-		if user 
-			if user.admin?
-				redirect_to admin_projects_path
-			else
-				logout
-				flash.now[:danger] = "Esta cuenta no es de administrador"
-				render 'new'
-			end
-		else
-			flash.now[:danger] = 'Contraseña y/o email incorrecto'
-			render 'new'	
-		end
+		token = get_token_from_code(params[:code], authorize_admin_url)
+        raw_information = token.get('https://graph.microsoft.com/v1.0/me').parsed
+
+        mail = raw_information["mail"] || raw_information["userPrincipalName"]
+
+        user = User.find_by(email: mail)
+        byebug
+
+        if user && user.admin?
+            auto_login(user)
+            redirect_to admin_profile_path
+        else
+        	flash.now[:danger] = 'Esta cuenta no es de administrador'
+        	render 'new'
+        end
 	end
 
 	def destroy
