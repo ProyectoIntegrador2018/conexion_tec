e37398f03abc32a277d21e76370640e82ea77be5
Santiago Sandoval <santiago_st_95@hotmail.com>
2019-04-24 19:49:53 -0500

    Mail view created

--- app/controllers/student/projects_controller.rb
+++ app/controllers/student/projects_controller.rb
@@ -31,7 +31,7 @@ class Student::ProjectsController < Student::BaseController
 		@project.status_id = Status.first.id
 
 		if @project.save
-			ApplicationMailer.with(user: current_user, project: @project).project_confirmation.deliver_now
+			UserMailer.with(user: current_user, project: @project).project_confirmation.deliver_now
 			flash[:success] = "Proyecto creado exitosamente!"
 			redirect_to action: 'index'
 		else
