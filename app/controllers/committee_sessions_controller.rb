28c543f263f54c0a67dd8e10080dad355198ec76
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2019-04-24 19:38:41 -0500

    Prepare pull from staging

--- app/controllers/committee_sessions_controller.rb
+++ app/controllers/committee_sessions_controller.rb
@@ -4,16 +4,16 @@ class CommitteeSessionsController < ApplicationController
     ITESM_MAIL = /^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(itesm|tec)\.mx$/
 
     def new
-        if current_user && current_user.committe?
-            redirect_to committe_profile_path
+        if current_user && current_user.committee?
+            redirect_to committee_profile_path
         else
-            @login_url = get_login_url(authorize_committe_url)
+            @login_url = get_login_url(authorize_committee_url)
         end
     end
 
-    # This method is in charge of redirecting to committe profile if info is valid
+    # This method is in charge of redirecting to committee profile if info is valid
     def create
-        token = get_token_from_code(params[:code], authorize_committe_url)
+        token = get_token_from_code(params[:code], authorize_committee_url)
         raw_information = token.get('https://graph.microsoft.com/v1.0/me').parsed
 
         mail = raw_information['mail'] || raw_information['userPrincipalName']
@@ -21,13 +21,14 @@ class CommitteeSessionsController < ApplicationController
 
         user = User.find_by(email: mail)
         if user # The user exists, login to the system
-            url = committe_profile_path
+            url = committee_profile_path
         else # The user is not on the database
-            url = committe_edit_path
+            url = committee_edit_path
             if mail.match(ITESM_MAIL) # Check if it is from the ITESM
-                user = create_committe(mail,full_name)
-            else #committe mail is not from ITESM 
-               #TODO implement error message
+                user = create_committee(mail,full_name)
+            else #committee mail is not from ITESM 
+                flash.now[:danger] = 'Correo invalido: favor de utilizar correo del ITESM.'
+                render 'new'
             end
         end
         login_redirect(user, url)
@@ -39,15 +40,15 @@ class CommitteeSessionsController < ApplicationController
         redirect_to url
     end
 
-    # Handle internal committe
-    def create_committe(mail,full_name)
+    # Handle internal committee
+    def create_committee(mail,full_name)
         # Create the user and login
-        committe = Committee.create()
+        committee = Committee.create()
         password = generates_password
         user = User.create(email: mail,
                             name: full_name,
                             userable_type: 'Committee',
-                            userable_id: committe.id,
+                            userable_id: committee.id,
                             password: password,
                             password_confirmation: password)
 
