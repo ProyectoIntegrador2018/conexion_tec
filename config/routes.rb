5a8e1e60a142214039479057347ad9dd2dfa1812
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2019-04-24 19:38:41 -0500

    Prepare pull from staging

--- config/routes.rb
+++ config/routes.rb
@@ -41,15 +41,6 @@ Rails.application.routes.draw do
   resources :questions
   resources :projects
 
-  get 'signup-proyecto', to: 'projects#new', as: :signup_project
-  post 'signup-proyecto', to: 'projects#create'
-
-  get 'signup-juez', to: 'judges#new', as: :signup_judge
-  post 'signup-juez', to: 'judges#create'
-
-  get 'signup-staff', to: 'staffs#new'
-  post 'signup-staff', to: 'staffs#create'
-
   namespace :judge do
     get 'profile', to: 'profile#index'
     get 'edit', to: 'profile#edit'
@@ -111,9 +102,38 @@ Rails.application.routes.draw do
     resources :users
   end
 
-  namespace :monitor do
-	  get 'projects', to: 'evaluation#index'
-	  patch 'evaluation', to: 'evaluation#update'
+  namespace :committee do
+    get 'profile', to: 'profile#show'
+    get 'profile/edit', to: 'profile#edit'
+    patch 'profile/edit', to: 'profile#update'
+    resources 'projects'
+    get 'judges', to: 'judges#index'
+    resources 'judges', only: [:destroy]
+    get 'evaluations', to: 'evaluations#index'
+    resources 'evaluations', only: [:destroy]
+    get 'questions', to: 'questions#show'
+    get 'questions/new', to: 'questions#new'
+    post 'questions/new', to: 'questions#create'
+    resources 'questions', only: [:edit, :update, :destroy]
+    get 'statistics/projects_category', to: 'statistics#projects_category'
+    get 'statistics/judges_expertise_areas', to: 'statistics#judges_expertise_areas'
+    get 'statistics/projects_score', to: 'statistics#projects_score'
+    get 'statistics/projects_score_category', to: 'statistics#projects_score_category'
+    get 'event_dates', to: 'event_dates#index'
+    get 'event_dates/new', to: "event_dates#new"
+    post 'event_dates/new', to: "event_dates#create"
+    delete 'event_dates/:id', to: "event_dates#destroy", as: :event_dates_destroy
+    post '/projects/approve/:id', to: "projects#approve", as: :approve_project
+    post '/projects/reject/:id', to: "projects#reject", as: :reject_project
+    post '/judges/approve/:id', to: "judges#approve", as: :approve_judge
+    post '/judges/reject/:id', to: "judges#reject", as: :reject_judge
+    resources :expertise_areas
+    resources :clients
+    resources :majors
+    resources :fields
+    resources :departments
+    resources :editions
+    resources :categories
   end
 
   namespace :student do
