9fa3bf6101cc8974bf70b9e23cb7f1871aff9adf
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2019-04-27 22:18:23 -0500

    Implemented jusges assistance

--- config/routes.rb
+++ config/routes.rb
@@ -110,6 +110,10 @@ Rails.application.routes.draw do
     post '/projects/reject/:id', to: "projects#reject", as: :reject_project
     post '/judges/approve/:id', to: "judges#approve", as: :approve_judge
     post '/judges/reject/:id', to: "judges#reject", as: :reject_judge
+    get '/projects_assistance', to: 'projects_assistance#index', as: :projects_assistance
+    patch '/projects_assistance/mark_assistance', to: 'projects_assistance#mark_assistance', as: :mark_assistance
+    get '/judges_assistance', to: 'judges_assistance#index', as: :judges_assistance
+    post '/judges_assistance/mark_assistance:id', to: 'judges_assistance#mark_assistance', as: :project_mark_assistance
     resources :expertise_areas
     resources :clients
     resources :majors
