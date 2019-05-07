397d134c7252e4315664805482532874ff796524
Santiago Sandoval <santiago_st_95@hotmail.com>
2019-05-07 09:44:37 -0500

    Mail prepared for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,9 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -91,4 +89,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'terralumx@gmail.com',
+    password: 'eCheverYa18',
+    authentication: 'plain',
+    enable_starttls_auto: true }
 end
