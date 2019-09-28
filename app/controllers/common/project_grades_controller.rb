4d0d76079002c9f91cb0fc5e77307dec51ecbc6c
JuanCarlosGzzPetrocchi <juancarlosgzz23@gmail.com>
2019-09-28 13:57:29 -0500

    Fix project grade view, add edit and update

--- app/controllers/common/project_grades_controller.rb
+++ app/controllers/common/project_grades_controller.rb
@@ -7,14 +7,32 @@ class Common::ProjectGradesController < Common::BaseController
   end
 
   def edit
-
+    @project = Project.find(params[:project_id])
+    @project_grade = ProjectGrade.find(params[:id])
+    @url = common_project_project_grade_path
   end
 
   def create
-
+    @project = Project.find(params[:project_id])
+    @project_grade = ProjectGrade.create(form_params)
+    @project.project_grade = @project_grade
+    flash[:success] = "Proyecto calificado"
+    redirect_to common_projects_path
   end
 
   def update
-
+    @project = Project.find(params[:project_id])
+    @project_grade = ProjectGrade.find(params[:id])
+    @project_grade.update(form_params)
+    @project.project_grade = @project_grade
+    flash[:success] = "Proyecto calificado"
+    redirect_to common_projects_path
   end
+
+  private
+
+  def form_params
+    params.require(:project_grade).
+    permit(:abstract_grade, :video_grade, :objective_grade)
+  end 
 end
