e9c1074ebdf85a7aba4d6b7076be02d190012bc7
José Eduardo Sánchez Yépiz <gallo.sanchez23@gmail.com>
2019-09-29 10:39:14 -0700

    Update app/controllers/student/projects_controller.rb

--- app/controllers/student/projects_controller.rb
+++ app/controllers/student/projects_controller.rb
@@ -109,7 +109,7 @@ class Student::ProjectsController < Student::BaseController
 				:video_url,
 				:description,
 				:email_professor,
-        :name_professor,
+				:name_professor,
         :semestrei,
         :social_impact,
 				:department_professor)
