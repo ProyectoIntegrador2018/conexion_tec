9b6c95c31720651a65ec0d86fedc5e848f2ed137
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-10-03 17:35:52 -0500

    Add validations for new abstract fields on projects model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,33 +1,38 @@
 class Project < ApplicationRecord
-
 	before_save :set_selection_score
 
 	validates :name, presence: true
-	validates :abstract, presence: true
-	validates :description, presence: true
 	validates :field_id, presence: true
-	validates :expertise_area_id, presence: true
 	validates :client_id, presence: true
+	validates :description, presence: true
+	validates :abstract_impact, presence: true
+	validates :abstract_problem, presence: true
+	validates :abstract_results, presence: true
+	validates :expertise_area_id, presence: true
+	validates :abstract_methodology, presence: true
+	validates :abstract_feasibility, presence: true
 
 	belongs_to :field
 	belongs_to :client
 	belongs_to :status
-	belongs_to :category
-	belongs_to :expertise_area
-	belongs_to :professor
 	belongs_to :student
 	belongs_to :edition
-	has_many :assignments
-  has_many :members
-  has_one :project_grade
+	belongs_to :category
+	belongs_to :professor
+	belongs_to :expertise_area
 	belongs_to :stand, optional: true
-	
+
+  has_many :members
+	has_many :assignments
+
+  has_one :project_grade
+
 	def set_selection_score
 		if self.selection_score.present? && self.status_id == Status.first.id
 			self.status_id = Status.second.id
 		elsif !self.selection_score.present?
 			self.status_id = Status.first.id
-		end 
+		end
 	end
 
 	def evaluations
