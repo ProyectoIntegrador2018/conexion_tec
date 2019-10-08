0f1b7690a73ba2c9f93387bfdfa8820e39fd47c0
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-10-07 21:59:08 -0500

    Add validations on ProjectGrade model

--- app/models/project_grade.rb
+++ app/models/project_grade.rb
@@ -1,3 +1,13 @@
 class ProjectGrade < ApplicationRecord
   belongs_to :project
+
+  validates :abstract,
+    :description,
+    :total_grade,
+    :abstract_impact,
+    :abstract_problem,
+    :abstract_results,
+    :abstract_methodology,
+    :abstract_feasibility,
+    presence: true
 end
