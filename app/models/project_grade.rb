4b946005ab70b1b6af74b39f538d725840723c77
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-10-10 15:29:41 -0500

    Fix rubocop issues

--- app/models/project_grade.rb
+++ app/models/project_grade.rb
@@ -12,16 +12,16 @@ class ProjectGrade < ApplicationRecord
             presence: true
 
   after_initialize do |project_grade|
-    get_total_grade
+    get_total_grade(project_grade)
   end
 
   after_update do |project_grade|
-    get_total_grade
+    get_total_grade(project_grade)
   end
 
   private
 
-  def get_total_grade project_grade
+  def get_total_grade(project_grade)
     project_grade.abstract.to_i +
     project_grade.description.to_i +
     project_grade.abstract_impact.to_i +
