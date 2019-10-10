d4bd1a0aa22617300ac14a03fff1f0790b608476
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-10-10 15:21:43 -0500

    Add after_initialize and after_update actions on ProjectGrade controller to fix rubocop issues

--- app/controllers/common/project_grades_controller.rb
+++ app/controllers/common/project_grades_controller.rb
@@ -14,7 +14,6 @@ class Common::ProjectGradesController < Common::BaseController
   def create
     @project = Project.find(params[:project_id])
     @project_grade = ProjectGrade.create(form_params)
-    @project_grade.total_grade = total_grade
     @project.project_grade = @project_grade
     flash[:success] = 'Proyecto calificado exitosamente'
     redirect_to common_projects_path
@@ -24,7 +23,6 @@ class Common::ProjectGradesController < Common::BaseController
     @project = Project.find(params[:project_id])
     @project_grade = ProjectGrade.find(params[:id])
     @project_grade.update(form_params)
-    @project_grade.update(total_grade: total_grade)
     @project.project_grade = @project_grade
     flash[:success] = 'Proyecto calificado exitosamente'
     redirect_to common_projects_path
@@ -41,15 +39,5 @@ class Common::ProjectGradesController < Common::BaseController
            :abstract_results,
            :abstract_methodology,
            :abstract_feasibility)
-  end
-
-  def total_grade
-    total_grade = params[:project_grade][:abstract].to_i +
-                  params[:project_grade][:description].to_i  +
-                  params[:project_grade][:abstract_impact].to_i +
-                  params[:project_grade][:abstract_problem].to_i +
-                  params[:project_grade][:abstract_results].to_i +
-                  params[:project_grade][:abstract_methodology].to_i +
-                  params[:project_grade][:abstract_feasibility].to_i
   end
 end
--- app/models/project_grade.rb
+++ app/models/project_grade.rb
@@ -10,4 +10,24 @@ class ProjectGrade < ApplicationRecord
             :abstract_methodology,
             :abstract_feasibility,
             presence: true
+
+  after_initialize do |project_grade|
+    project_grade.total_grade = project_grade.abstract.to_i +
+                                project_grade.description.to_i +
+                                project_grade.abstract_impact.to_i +
+                                project_grade.abstract_problem.to_i +
+                                project_grade.abstract_results.to_i +
+                                project_grade.abstract_methodology.to_i +
+                                project_grade.abstract_feasibility.to_i
+  end
+
+  after_update do |project_grade|
+    project_grade.total_grade = project_grade.abstract.to_i +
+                                project_grade.description.to_i +
+                                project_grade.abstract_impact.to_i +
+                                project_grade.abstract_problem.to_i +
+                                project_grade.abstract_results.to_i +
+                                project_grade.abstract_methodology.to_i +
+                                project_grade.abstract_feasibility.to_i
+  end
 end
