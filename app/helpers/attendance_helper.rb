b1dece21ba80347b0de63a714f2ea1a5a60e7e69
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-10-14 15:48:57 -0500

    Refactor attendance helper

--- app/helpers/attendance_helper.rb
+++ app/helpers/attendance_helper.rb
@@ -1,37 +1,19 @@
 module AttendanceHelper
-    def judgeAttendance(judge)
-        if judge.attended
-            judge.attended = false
-            judge.save
-            false
-        else
-            judge.attended = true
-            judge.save
-            true
-        end
-    end
+  def judge_attendance(judge)
+    judge.attended = !judge.attended
+    judge.save
+    return judge.attended
+  end
 
-    def projectAttendance(project)
-        if project.attended == 1
-            project.attended = 0
-            project.save
-            false
-        elsif project.attended == 0
-            project.attended = 1
-            project.save
-            true
-        end
-    end
+  def project_attendance(project)
+    project.attended = project.attended == 0 ? 1 : 0
+    project.save
+    return project.attended == 1
+  end
 
-    def blockProject(project)
-        if project.attended == 1
-            project.attended = 2
-            project.save
-            true
-        elsif project.attended == 2
-            project.attended = 1
-            project.save
-            false
-        end
-    end
+  def block_project(project)
+    project.attended = project.attended == 1 ? 2 : 1
+    project.save
+    return project.attended == 2
+  end
 end
