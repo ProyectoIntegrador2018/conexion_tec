0d18a7ccdee458298ddda892eb5ddb6cd89f6bc0
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-10-14 16:40:49 -0500

    Fiz rubocop issues

--- app/helpers/attendance_helper.rb
+++ app/helpers/attendance_helper.rb
@@ -1,21 +1,18 @@
 module AttendanceHelper
   def judge_attendance(judge)
-    attended = judge.attended
-    judge.attended = !attended
+    judge.attended = !judge.attended
     judge.save
     judge.attended
   end
 
   def project_attendance(project)
-    attended = project.attended
-    project.attended = attended == 0 ? 1 : 0
+    project.attended = project.attended.zero? ? 1 : 0
     project.save
     project.attended == 1
   end
 
   def block_project(project)
-    attended = project.attended
-    project.attended = attended == 1 ? 2 : 1
+    project.attended = project.attended == 1 ? 2 : 1
     project.save
     project.attended == 2
   end
