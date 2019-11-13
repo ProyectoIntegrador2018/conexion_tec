5597ada49b081bcf05018e3293b2965a7d041a40
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-11-13 17:41:08 -0600

    Move current_edition function from editions to applications helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,22 @@
 module ApplicationHelper
-	require	'openssl'
+  require 'openssl'
 
-	def self.encrypt(string)
-		cipher = OpenSSL::Cipher.new('DES-EDE3-CBC').encrypt
-		@@key = cipher.random_key
-		cipher.key = @@key
-		s = cipher.update(string) + cipher.final
-		s.unpack('H*')[0].upcase
-	end
+  def self.encrypt(string)
+    cipher = OpenSSL::Cipher.new('DES-EDE3-CBC').encrypt
+    @@key = cipher.random_key
+    cipher.key = @@key
+    s = cipher.update(string) + cipher.final
+    s.unpack('H*')[0].upcase
+  end
 
-	def self.decrypt(string)
-		cipher = OpenSSL::Cipher.new('DES-EDE3-CBC').decrypt
-		cipher.key = @@key
-		s = [string].pack("H*").unpack("C*").pack("c*")
-		cipher.update(s) + cipher.final
-  	end
+  def self.decrypt(string)
+    cipher = OpenSSL::Cipher.new('DES-EDE3-CBC').decrypt
+    cipher.key = @@key
+    s = [string].pack("H*").unpack("C*").pack("c*")
+    cipher.update(s) + cipher.final
+  end
+
+  def current_edition
+    Edition.last
+  end
 end
--- app/helpers/common/editions_helper.rb
+++ app/helpers/common/editions_helper.rb
@@ -1,8 +1,4 @@
 module Common::EditionsHelper
-  def current_edition
-    Edition.last
-  end
-
   def parse_date(date)
     date.present? ? Date.strptime(date, '%d/%m/%Y') : ''
   end
