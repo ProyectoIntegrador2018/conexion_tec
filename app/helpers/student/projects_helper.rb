d4f533a98a908bea20cfcf8c677afbb5c95fc9a3
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-11-13 18:16:07 -0600

    Refactor can_register_project function on Student::ProjectsHelper to solve code climate issues

--- app/helpers/student/projects_helper.rb
+++ app/helpers/student/projects_helper.rb
@@ -4,15 +4,12 @@ module Student::ProjectsHelper
     starting_date = current_edition[:project_registration_starting_date]
     ending_date = current_edition[:project_registration_ending_date]
     today = Date.today
-    if starting_date && ending_date
-      can_register = starting_date <= today and today <= ending_date
-    elsif starting_date
-      can_register = starting_date <= today
-    elsif ending_date
-      can_register = today <= ending_date
-    else
-      can_register = true
-    end
+
+    can_register = true
+    if starting_date
+      can_register &= starting_date <= today
+    if ending_date
+      can_register &= today <= ending_date
     can_register
   end
 end
